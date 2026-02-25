ce15c5ed8d23da6ee7d6e8691b1e8f95b13a0151
William Manoel <williammartins19@outlook.com>
2026-02-25 13:46:02 -0300

    Update Portifólio

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-document.querySelectorAll('.sidebar nav a').forEach(anchor => {
+// Scroll suave ao clicar no menu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+        document.querySelector(this.getAttribute('href'))
+            .scrollIntoView({ behavior: 'smooth' });
     });
 });
