4539311e95abdd0137ef4a01825c077f11baefd2
William Manoel <williammartins19@outlook.com>
2026-02-25 11:58:23 -0300

    Update Portifólio

---
+++ index.js
@@ -0,0 +1,9 @@
+const links = document.querySelectorAll("nav a");
+
+links.forEach(link => {
+    link.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        target.scrollIntoView({ behavior: "smooth" });
+    });
+});
